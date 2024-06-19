33e69c65b18ca326a195be30b8ff5cb7686b28ed
I0-o-0I <perchik054@gmail.com>
2024-06-19 13:38:55 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,11 @@
+immutable_var = 1, 2, "ttt"
+print("Immutable var:", immutable_var)
+# Кортеж – это неизменяемая упорядоченная коллекция, которая может содержать в себе разные типы данных.
+# К неизменяемым объектам относятся числа и строки. В данном случае: 1, 2, "ttt"
+
+mutable_list = [1, 2, "ttt"]
+mutable_list[1] = 5
+mutable_list.append("qqq")
+print("Mutable list:", mutable_list)
+
+
